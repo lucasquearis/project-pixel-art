70a1843889f0c9492acbe7f4db3a16693510bdc8
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-05 17:06:01 -0300

    requisito 1 no JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Exercício 1
+const selecBody = document.getElementById('body')
+const createH1 =document.createElement('h1');
+createH1.innerHTML ='Paleta de Cores'
+createH1.id = 'title'
+selecBody.appendChild(createH1);
+
+// 
