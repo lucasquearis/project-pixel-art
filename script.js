888e6a00764167d71f3dae344163aba45ec68898
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-05 18:05:55 -0300

    requisito 2 e 3 no JS

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Exercício 1
-const selecBody = document.getElementById('body')
-const createH1 =document.createElement('h1');
+const selecBody = document.getElementById('body');
+const createH1 = document.createElement('h1');
 createH1.innerHTML ='Paleta de Cores'
 createH1.id = 'title'
 selecBody.appendChild(createH1);
 
-// 
+// Exercicio 2
+const createTable = document.createElement('table');
+createTable.id = 'color-palette'
+selecBody.appendChild(createTable);
+
+const selectTable = document.getElementById('color-palette');
+
+
+for(index = 1; index < 5; index += 1){
+  const createTh = document.createElement('th');
+  selectTable.appendChild(createTh);
+  createTh.className = 'color '
+}
+
+const arrayCor = ['black', 'two', 'three', 'four'];
+const selecTh = document.querySelectorAll('.color ')
+
+for(index = 0; index < arrayCor.length; index += 1){
+  selecTh[index].className = 'color '+arrayCor[index];
+}
+
+
+
+
