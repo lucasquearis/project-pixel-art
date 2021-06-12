e1925cfdf20dcc1e7ad223cb0d7a957e2d355a3c
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-12 18:08:44 -0300

    requisito 8 e 9 refatorados

--- script.js
+++ script.js
@@ -71,106 +71,37 @@ const selectColorPalltet = () => {
   });
 };
 
-// const criarBotao = document.createElement('button');
-// selecBody.appendChild(criarBotao);
-// criarBotao.id = 'clear-board';
-// criarBotao.innerHTML = 'Limpar';
-// criarBotao.addEventListener('click', () => {
-//   const numeroMaximoQuadrado = (numeroQuadrado * numeroQuadrado);
-//   const selecPixel = document.querySelectorAll('.pixel');
-//   for (let index = 0; index < numeroMaximoQuadrado; index += 1) {
-//     selecPixel[index].style.backgroundColor = 'white';
-//   }
-// });
+const squarePaint = () => {
+  const pickupSquare = document.querySelectorAll('.pixel');
+  pickupSquare.forEach((square) => {
+    square.addEventListener('click', (event) => {
+      const click = event;
+      const pickupColor = window.getComputedStyle(document
+        .querySelector('.selected')).getPropertyValue('background-color');
+      const condicao = click.target.style.backgroundColor === pickupColor;
+      if (condicao) {
+        click.target.removeAttribute('style');
+      } else {
+        click.target.style.backgroundColor = pickupColor;
+      }
+    });
+  });
+};
 
-// const corSelecionada = window
-//   .getComputedStyle(document.querySelector('.selected')).getPropertyValue('background-color');
-
-// function mudarCorPixel() {
-//   const numeroMaximoQuadrado = (numeroQuadrado * numeroQuadrado);
-//   const selecPixel = document.querySelectorAll('.pixel');
-//   for (let index2 = 0; index2 < numeroMaximoQuadrado; index2 += 1) {
-//     selecPixel[index2].style.backgroundColor = 'white';
-//   }
-//   for (let index = 0; index < numeroMaximoQuadrado; index += 1) {
-//     selecPixel[index].addEventListener('click', (event) => {
-//       if (event.target.style.backgroundColor === corSelecionada) {
-//         event.target.style.backgroundColor = 'white';
-//       } else {
-//         event.target.style.backgroundColor = corSelecionada;
-//       }
-//     });
-//   }
-// }
-
-// function valorInput() {
-//   const numeroQuadrado2 = document.getElementById('board-size').value;
-//   if (numeroQuadrado2 === '') {
-//     alert('Board inválido!');
-//   } else if (numeroQuadrado2 < 5) {
-//     createQuadrado.innerHTML = '';
-//     fazerQuadrados(5);
-//     mudarCorPixel();
-//   } else if (numeroQuadrado2 > 7) {
-//     createQuadrado.innerHTML = '';
-//     fazerQuadrados(7);
-//     mudarCorPixel();
-//   } else {
-//     createQuadrado.innerHTML = '';
-//     fazerQuadrados(numeroQuadrado);
-//     mudarCorPixel();
-//   }
-// }
-
-// const input = document.createElement('input');
-
-// selecBody.appendChild(input);
-// input.id = 'board-size';
-// const botaoTamanho = document.createElement('button');
-// selecBody.appendChild(botaoTamanho);
-// botaoTamanho.id = 'generate-board';
-// botaoTamanho.innerHTML = 'VQV';
-// botaoTamanho.addEventListener('click', valorInput);
-
-// fazerQuadrados(5);
-
-// const selectPreto = 'color black selected';
-// const selectTwo = 'color two selected';
-// const selectThree = 'color three selected';
-// const selectFour = 'color four selected';
-// const colorBlack = 'color black';
-// const colorTwo = 'color two';
-// const colorThree = 'color three';
-// const colorFour = 'color four';
-// selecTh[0].addEventListener('click', () => {
-//   selecTh[0].className = selectPreto;
-//   selecTh[1].className = colorTwo;
-//   selecTh[2].className = colorThree;
-//   selecTh[3].className = colorFour;
-// });
-
-// selecTh[1].addEventListener('click', () => {
-//   selecTh[0].className = colorBlack;
-//   selecTh[1].className = selectTwo;
-//   selecTh[2].className = colorThree;
-//   selecTh[3].className = colorFour;
-// });
-
-// selecTh[2].addEventListener('click', () => {
-//   selecTh[0].className = colorBlack;
-//   selecTh[1].className = colorTwo;
-//   selecTh[2].className = selectThree;
-//   selecTh[3].className = colorFour;
-// });
-
-// selecTh[3].addEventListener('click', () => {
-//   selecTh[0].className = colorBlack;
-//   selecTh[1].className = colorTwo;
-//   selecTh[2].className = colorThree;
-//   selecTh[3].className = selectFour;
-// });
-
-// Exercício 8
+const createButton = () => {
+  const newButton = document.createElement('button');
+  newButton.id = 'clear-board';
+  newButton.innerText = 'Limpar';
+  const selectPallet = document.querySelector('#color-palette');
+  selectPallet.insertAdjacentElement('afterend', newButton);
+  newButton.addEventListener('click', () => {
+    const findPixel = document.querySelectorAll('.pixel');
+    findPixel.forEach((square) => {
+      const eachSquare = square;
+      eachSquare.removeAttribute('style');
+    });
+  });
+};
 
 window.onload = () => {
   createTitle();
@@ -178,4 +109,6 @@ window.onload = () => {
   classnameForPallet();
   criarQuadrado(5);
   selectColorPalltet();
+  squarePaint();
+  createButton();
 };
